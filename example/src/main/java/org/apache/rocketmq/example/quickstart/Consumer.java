8b2a8c44fff6ddff4bcf87cc681b5b43e4d317de
suntianyu <suntianyu@shuwen.com>
2020-10-30 11:37:09 +0800

    refactor(example): 修改示例中consumer的配置信息用以本地测试

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
@@ -35,7 +35,7 @@ public class Consumer {
         /*
          * Instantiate with specified consumer group name.
          */
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name_4");
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("CID_DEFAULT_TEST");
 
         /*
          * Specify name server addresses.
@@ -44,11 +44,11 @@ public class Consumer {
          * Alternatively, you may specify name server addresses via exporting environmental variable: NAMESRV_ADDR
          * <pre>
          * {@code
-         * consumer.setNamesrvAddr("name-server1-ip:9876;name-server2-ip:9876");
+         *
          * }
          * </pre>
          */
-
+        consumer.setNamesrvAddr("127.0.0.1:9876");
         /*
          * Specify where to start in case the specified consumer group is a brand new one.
          */
@@ -57,7 +57,7 @@ public class Consumer {
         /*
          * Subscribe one more more topics to consume.
          */
-        consumer.subscribe("TopicTest", "*");
+        consumer.subscribe("RMQ_SYS_TRANS_HALF_TOPIC", "*");
 
         /*
          *  Register callback to execute on arrival of messages fetched from brokers.
